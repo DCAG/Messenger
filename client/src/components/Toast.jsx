fd7a327a9cae906198ebcdc38c00eaf70b174945
Amir Granot <digiclick@hotmail.com>
2024-07-22 13:44:56 +0300

    fixed toast display grow shrink and ellipsis for overflow

--- client/src/components/Toast.jsx
+++ client/src/components/Toast.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Toast({ payload, show, type }) {
-  if (!show) {
+function Toast({ show, type, payload }) {
+  const [showToast, setShowToast] = useState(show)
+  useEffect(() => {
+    setShowToast(show)
+  }, [show])
+
+  if (!showToast) {
     return
   }
   if (type === 'connection') {
     return (
       <div className="toast-container">
-        <div className="toast-message">
-          {payload} connected!
-        </div>
+        <span className='toast-message span1'>{payload} connected!</span>
+        <span className="toast-close" onClick={() => setShowToast(false)}>{"\u00d7"}</span>
       </div>
     )
   }
   else if (type === 'message') {
     return (
       <div className="toast-container">
-        <div className="toast-message">
-          {`${payload?.contactName} (${payload?.chatName}) says: `}<br />
-          {payload?.message}
-        </div>
+        <span className='toast-sender'>{`${payload?.contactName} ${payload?.chatName ? '(' + payload?.chatName + ')' : ''} says: `}</span>
+        <span className="toast-close" onClick={() => setShowToast(false)}>{"\u00d7"}</span>
+        <span className='toast-message'>{payload?.message}</span>
       </div>
     )
   }
