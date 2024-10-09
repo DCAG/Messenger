4e24a97316eb4fa2638cd67c457adbd23b5c2370
Amir Granot <digiclick@hotmail.com>
2024-10-09 21:43:21 +0300

    docker and deployment updates

---
+++ DeploymentNotes.md
@@ -0,0 +1,6 @@
+# Notes
+
+Using Azure App Service Docker Compose (Preview) to host this app.
+There are caveats, not everything is working as expected.
+
+[Configure multi-container apps on Microsoft Learn](https://learn.microsoft.com/en-us/azure/app-service/configure-custom-container?tabs=debian&pivots=container-linux#configure-multi-container-apps)
--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -2,11 +2,25 @@ const mongoose = require('mongoose');
 
 const connectDB = () => {
     // Connect to mongoDB database
-    const MONGODB_CONN_STR = process.env.AZURE_COSMOS_CONNECTIONSTRING || process.env.MONGODB_CONN_STR
-    mongoose
-    .connect(MONGODB_CONN_STR)
-    .then((db) => {
-        console.log(`Connected to ${MONGODB_CONN_STR}`)
+    const mongoDBUris = process.env.MONGODB_CONN_STR.split(',')
+    let p = Promise.reject(mongoDBUris)
+    for(var i = 0; i < mongoDBUris.length; i++){
+        p = p.catch( async (reason) => {
+            console.log("attempting to connect to mongodb uri: " + reason[0])
+            try{
+                return await mongoose.connect(reason[0])
+            }catch{
+                if(reason.length > 1){
+                    return Promise.reject(reason.splice(1))
+                }
+                else{
+                    throw new Error('failed to connect to all provided mongodb addresses')
+                }
+            }
+        })
+    }
+    p.then((db) => {
+        console.log(`Connected to ${db.connections[0]._connectionString}`)
     })
     .catch((error) => console.log(error))
 }
